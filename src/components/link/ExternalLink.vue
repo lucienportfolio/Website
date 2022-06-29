6e60ffada15f4d1d5411e09f29b4f68ba500420f
Garfield550 <openpgp@550.moe>
2022-06-29 11:56:42 +0000

    feat: add external link component

---
+++ src/components/link/ExternalLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  to: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <a :href="to" target="_blank" rel="noopener">
+    <slot />
+  </a>
+</template>
