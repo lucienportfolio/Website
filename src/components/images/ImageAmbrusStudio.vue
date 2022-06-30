9b7f8377fbf1344d48845a14272ddc9b566db566
Garfield550 <openpgp@550.moe>
2022-06-30 09:42:21 +0000

    feat: add ambrus studio logo

---
+++ src/components/images/ImageAmbrusStudio.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Logo1x from '@/assets/images/brand/ambrus-studio-logo.png'
+import Logo2x from '@/assets/images/brand/ambrus-studio-logo@2x.png'
+import Logo3x from '@/assets/images/brand/ambrus-studio-logo@3x.png'
+
+const image = Logo1x
+const imageSet = `${Logo1x}, ${Logo2x} 2x, ${Logo3x} 3x`
+</script>
+
+<template>
+  <img
+    class="w-240px h-80px select-none"
+    :src="image"
+    alt="Ambrus Studio Logo"
+    :srcset="imageSet"
+  />
+</template>
