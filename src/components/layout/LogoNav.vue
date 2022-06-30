06f5526dc0548c93fda9082dd5dd014496f5766e
Garfield550 <openpgp@550.moe>
2022-06-30 09:41:48 +0000

    refactor: rename component

--- src/components/layout/LogoNav.vue
+++ src/components/layout/LogoNav.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import IconFavicon from '../icons/IconFavicon.vue'
+import IconAmbrus from '../icons/IconAmbrus.vue'
 </script>
 
 <template>
   <RouterLink to="/" title="Home">
-    <IconFavicon class="h-40px w-40px text-white mr-16px" />
+    <IconAmbrus class="h-40px w-40px text-white mr-16px" />
   </RouterLink>
 </template>
