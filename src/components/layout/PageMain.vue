76d2bcf539f8b0fd9a1ffbc70762a837af9e5b88
Garfield550 <openpgp@550.moe>
2022-07-04 12:39:52 +0000

    feat: add page background image

---
+++ src/components/layout/PageMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <main id="main" class="bg-black-bg bg-cover bg-top bg-repeat-y">
+    <div class="max-w-1264px mx-auto">
+      <slot />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#main {
+  background-image: url('~@/assets/images/bg/bg-page.png');
+  background-image: image-set(
+    url('~@/assets/images/bg/bg-page.png') 1x,
+    url('~@/assets/images/bg/bg-page@2x.png') 2x,
+    url('~@/assets/images/bg/bg-page@3x.png') 3x
+  );
+}
+</style>
