514ead9db6b73418cbca11a0263362cbce843bc5
Garfield550 <openpgp@550.moe>
2022-07-05 11:59:50 +0000

    feat: mobile styles

--- src/components/mint/NFTBanner.vue
+++ src/components/mint/NFTBanner.vue
@@ -20,7 +20,7 @@ const imageSet = computed(() => {
 <template>
   <div class="flex flex-row flex-nowarp items-center">
     <img
-      class="w-full h-480px select-none"
+      class="w-full h-auto md:w-auto md:h-480px select-none"
       :src="image"
       alt="NFT Item Banner"
       :srcset="imageSet"
--- src/components/mint/NFTDisclaimer.vue
+++ src/components/mint/NFTDisclaimer.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import 'swiper/css'
+import 'swiper/css/autoplay'
 import 'swiper/css/pagination'
-import { Pagination } from 'swiper'
+import { Autoplay, Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { computed } from 'vue'
 
@@ -15,18 +16,21 @@ const images = computed(() => {
   if (Array.isArray(props.images)) return props.images
   return [props.images]
 })
-const swiperModules = [Pagination]
+const swiperModules = [Autoplay, Pagination]
 </script>
 
 <template>
-  <div class="flex flex-col w-652px">
+  <div class="flex flex-col w-full md:w-652px">
     <Swiper
-      class="c-swiper"
+      class="disclaimer-swiper"
+      :loop="true"
+      :slides-per-view="1"
       :modules="swiperModules"
+      :autoplay="{ delay: 5000 }"
       :pagination="{ enabled: true, clickable: true }"
     >
       <SwiperSlide v-for="(image, index) in images" :key="index" :virtual-index="index">
-        <img class="w-652px h-652px select-none" :src="image" alt="Disclaimer Image" />
+        <img class="w-full h-auto select-none" :src="image" alt="Disclaimer Image" />
       </SwiperSlide>
     </Swiper>
     <div class="p-24px bg-white/10 text-12px leading-20px text-grey-medium">
@@ -37,16 +41,18 @@ const swiperModules = [Pagination]
 </template>
 
 <style>
-.c-swiper {
-  @apply m-0 w-652px h-652px;
+.disclaimer-swiper {
+  @apply m-0 w-full h-auto;
 }
-.c-swiper > .swiper-pagination {
-  @apply left-24px  bottom-24px w-auto text-left;
+.disclaimer-swiper > .swiper-pagination {
+  @apply left-24px bottom-24px w-auto text-left;
 }
-.c-swiper > .swiper-pagination > .swiper-pagination-bullet {
-  @apply w-60px h-2px bg-none bg-white opacity-50 rounded-none;
+.disclaimer-swiper > .swiper-pagination > .swiper-pagination-bullet {
+  @apply w-30px md:w-60px h-2px bg-none bg-white opacity-50 rounded-none;
 }
-.c-swiper > .swiper-pagination > .swiper-pagination-bullet.swiper-pagination-bullet-active {
+.disclaimer-swiper
+  > .swiper-pagination
+  > .swiper-pagination-bullet.swiper-pagination-bullet-active {
   @apply opacity-100;
 }
 </style>
