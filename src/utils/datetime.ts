f4e5e84ea4e9a3de30dc529c132bb81da671e178
Garfield550 <openpgp@550.moe>
2022-07-06 13:10:18 +0000

    feat: add datetime utils

---
+++ src/utils/datetime.ts
@@ -0,0 +1,12 @@
+import { DateTime } from 'luxon'
+
+export function formatDatetime(seconds: number, format?: string): string {
+  if (!format) format = 'DDD'
+  return DateTime.fromSeconds(seconds).toFormat(format)
+}
+
+export function isHistorical(seconds: number): boolean {
+  const dateTime = DateTime.fromSeconds(seconds)
+  const nowEpoch = DateTime.now().toSeconds()
+  return nowEpoch > dateTime.toSeconds()
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './array'
 export * from './image'
+export * from './datetime'
