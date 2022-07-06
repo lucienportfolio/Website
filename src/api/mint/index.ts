93283ab667e995404987d79c4a83045bf04382f0
Garfield550 <openpgp@550.moe>
2022-07-06 13:09:47 +0000

    feat: add api types

--- src/api/mint/index.ts
+++ src/api/mint/index.ts
@@ -2,6 +2,8 @@ import type { NFTItem } from '@/types'
 
 const demoNFTItem: NFTItem = {
   id: 1,
+  availableTimestamp: 1657111916, // Old time
+  // availableTimestamp: 1660147200, // Thursday, August 11, 2022 12:00:00 AM GMT+08:00
   bannerImages: [
     'http://localhost:3000/demo/images/nft-banner-thorn.png',
     'http://localhost:3000/demo/images/nft-banner-thorn@2x.png',
@@ -16,6 +18,35 @@ const demoNFTItem: NFTItem = {
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id vestibulum diam. Sed aliquet dolor leo, eu facilisis magna porta vel. Etiam vulputate augue ac odio suscipit interdum. Donec nec auctor lectus, eu molestie odio. Nullam euismod, felis eu venenatis tincidunt, arcu enim dictum est, quis viverra nisl est quis est. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tincidunt est nec tortor pellentesque finibus. Suspendisse lacinia eu metus id lacinia. Nullam elementum nisl dui, ut iaculis est tristique eget. Suspendisse odio magna, gravida ultricies molestie in, blandit vel dui. Nam convallis ante lacus, a interdum orci bibendum ut. Sed placerat augue vehicula diam bibendum commodo. Sed a porta orci.'
   },
+  editions: [
+    {
+      name: 'Ultimate Edition',
+      value: 'ultimate',
+      amount: 0,
+      total: 1086,
+      price: 1.2,
+      style: { background: 'linear-gradient(90deg, #202020 0%, #414141 100%)', boxShadow: '' }
+    },
+    {
+      name: 'Legendary Edition',
+      value: 'legendary',
+      amount: 15,
+      total: 1086,
+      price: 0.6,
+      style: {
+        background: 'linear-gradient(90deg, #E4AA15 0%, #F0C75D 100%)',
+        boxShadow: '0px 0px 8px 1px #FFB800, inset 4px 0px 10px rgba(0, 0, 0, 0.15)'
+      }
+    },
+    {
+      name: 'Epic Edition',
+      value: 'epic',
+      amount: 10,
+      total: 1086,
+      price: 0.2,
+      style: { background: 'linear-gradient(90deg, #399E98 0%, #88C4C0 100%)', boxShadow: '' }
+    }
+  ],
   introduction: [
     {
       title: 'NFT Sale',
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,30 +1,79 @@
 export type NFTItemDisclaimer = {
+  /** 商品信息图片列表 */
   images: string | string[]
+  /** 免责声明正文 */
   content: string
 }
 
 export type NFTItemIntro = {
+  /** 商品介绍段落标题 */
   title: string
+  /** 商品介绍正文，支持 \n 换行 */
   content: string
 }
 
 export type NFTPropertyMode = 'mode-3' | 'mode-6' | 'mode-9'
 
 export type NFTPropertyData = {
+  /** 属性名称 */
   key: string
+  /** 属性值 */
   value: string
 }
 
 export type NFTItemProperty = {
+  /** 属性排列方式（一列 mode-3，两列 mode-6，三列 mode-9） */
   mode: NFTPropertyMode
+  /** 属性段落标题 */
   title: string
+  /** 属性数据 */
   data: NFTPropertyData[]
 }
 
+export type NFTItemEditionStyle = {
+  /**
+   * 商品类型颜色渐变，CSS 字符串，
+   * 如 "linear-gradient(90deg, #E4AA15 0%, #F0C75D 100%)"
+   */
+  background: string
+  /**
+   * 商品类型发光效果，CSS 字符串，
+   * 如 "0px 0px 8px 1px #FFB800, inset 4px 0px 10px rgba(0, 0, 0, 0.15)"
+   */
+  boxShadow: string
+}
+
+export type NFTItemEdition = {
+  /** 商品类型名称 */
+  name: string
+  /** 商品类型值（用于 API 传参） */
+  value: string
+  /** 商品类型剩余数量，为 0 显示 "Sold Out" */
+  amount: number
+  /** 商品类型总量，用于展示 "<amount>/<total> Left" */
+  total: number
+  /** 商品类型价格 */
+  price: number
+  /** 商品类型颜色 */
+  style: NFTItemEditionStyle
+}
+
 export type NFTItem = {
+  /** NFT 的唯一 ID */
   id: string | number
+  /** NFT 发售时间，UNIX 时间戳，前端会进行时区换算 */
+  availableTimestamp: number
+  /**
+   * NFT Banner 图，为列表的时候为多分辨率图片，
+   * 文件名需包含 @ 符号，如 @1x @2x @3x
+   */
   bannerImages: string | string[]
+  /** NFT 商品信息和免责声明 */
   disclaimer: NFTItemDisclaimer
+  /** NFT 商品类型 */
+  editions: NFTItemEdition[]
+  /** NFT 商品介绍（第一部分） */
   introduction: NFTItemIntro[]
-  properties: NFTItemProperty[]
+  /** NFT 属性介绍（第二部分） */
+  properties?: NFTItemProperty[]
 }
