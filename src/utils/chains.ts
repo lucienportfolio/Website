6ce332f3a2e2be143165b5b0fd5cef393d20cddb
Garfield550 <openpgp@550.moe>
2022-07-11 09:53:54 +0000

    feat: add chain utils

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -1,4 +1,9 @@
-import { CHAIN_IDS_TO_NAMES, CHAIN_INFO, SupportedChainId } from '@/constants/chains'
+import {
+  CHAIN_IDS_TO_NAMES,
+  CHAIN_INFO,
+  DefaultChainId,
+  SupportedChainId
+} from '@/constants/chains'
 import type { ChainInfo } from '@/constants/chains'
 
 export function getChainIdFromName(name: string): number | undefined {
@@ -18,4 +23,28 @@ export function getChainInfoFromName(name: string): ChainInfo | undefined {
 
 export function getChainInfoFromId(id: string | number): ChainInfo | undefined {
   return CHAIN_INFO[id as SupportedChainId]
+}
+
+export function getChainInfo(): ChainInfo {
+  const chainId: string | undefined = import.meta.env.VITE_CHAIN_ID
+  if (!chainId) console.warn('VITE_CHAIN_ID not set')
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return getChainInfoFromId(chainId || DefaultChainId)!
+}
+
+export function getInfuraId(): string | undefined {
+  const infuraId = import.meta.env.VITE_INFURA_API_KEY
+  if (!infuraId) throw new TypeError('VITE_INFURA_API_KEY not set')
+  return infuraId
+}
+
+export function getInfuraUrl(id: string | number): string | undefined {
+  const chainInfo = getChainInfoFromId(id)
+  if (!chainInfo) return undefined
+  if (chainInfo.infuraNameKey) {
+    const infuraId = getInfuraId()
+    return `https://${chainInfo.infuraNameKey}.infura.io/v3/${infuraId}`
+  }
+  if (chainInfo.rpcUrl) return chainInfo.rpcUrl
+  return undefined
 }
