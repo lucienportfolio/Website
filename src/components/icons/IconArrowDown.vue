e79bfd659b82fb3fb904c4f9cc2beffd9e784b9a
Garfield550 <openpgp@550.moe>
2022-07-12 13:38:31 +0000

    feat: add new icon

---
+++ src/components/icons/IconArrowDown.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    width="8"
+    height="5"
+    viewBox="0 0 8 5"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 0.79554L0.807572 0L4 3.35224L7.19243 0L8 0.79554L4.00003 5L0 0.79554Z" />
+  </svg>
+</template>
