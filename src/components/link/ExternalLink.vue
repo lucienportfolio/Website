36081610281fbb0cc1603f887caf3522322b387c
Garfield550 <openpgp@550.moe>
2022-07-12 13:40:48 +0000

    feat: support change target

--- src/components/link/ExternalLink.vue
+++ src/components/link/ExternalLink.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 interface Props {
   to: string
+  blank?: boolean
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const target = computed(() => (props.blank ? '_blank' : undefined))
+const rel = computed(() => (props.blank ? 'noopener' : undefined))
 </script>
 
 <template>
-  <a :href="to" target="_blank" rel="noopener">
+  <a :href="to" :target="target" :rel="rel">
     <slot />
   </a>
 </template>
