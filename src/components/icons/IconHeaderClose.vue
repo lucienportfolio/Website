4e0c157fc51b5338e539bc808809755157f57fde
Garfield550 <openpgp@550.moe>
2022-07-13 13:01:03 +0000

    feat: add icons

---
+++ src/components/icons/IconHeaderClose.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1.11292 0L20 18.9163L18.9507 19.9672L0.063634 1.0509L1.11292 0Z" />
+    <path d="M0 18.9492L18.8871 0.0328661L19.9363 1.08377L1.04928 20.0001L0 18.9492Z" />
+  </svg>
+</template>
---
+++ src/components/icons/IconHeaderMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="24"
+    height="20"
+    viewBox="0 0 24 20"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 0H24V1.33333H0V0Z" />
+    <path d="M0 9.33331H24V10.6666H0V9.33331Z" />
+    <path d="M0 18.6667H24V20H0V18.6667Z" />
+  </svg>
+</template>
