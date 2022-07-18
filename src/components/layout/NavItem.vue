810c72517865147978b9f60870c10e740a533e51
Garfield550 <openpgp@550.moe>
2022-07-18 07:36:58 +0000

    feat: add soon for games nav

--- src/components/layout/NavItem.vue
+++ src/components/layout/NavItem.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 interface Props {
   className?: string
+  soon?: boolean
+}
+interface Emits {
+  (e: 'onNavClick'): void
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+const navClass = computed(() => {
+  return {
+    soon: props.soon,
+    [`${props.className}`]: props.className
+  }
+})
+const handleNavClick = () => {
+  if (props.soon) return
+  emit('onNavClick')
+}
 </script>
 
 <template>
   <div
-    class="nav-item flex flex-row flex-nowrap justify-center items-center h-80px xl:h-auto px-24px cursor-pointer"
-    :class="[className]"
+    class="nav-item relative flex flex-row flex-nowrap justify-center items-center h-80px xl:h-auto px-24px cursor-pointer"
+    :class="navClass"
+    @click.stop.prevent="handleNavClick"
   >
     <div class="nav-item__line absolute left-0 h-2px w-0 bg-rust" />
     <span
@@ -17,24 +35,30 @@ defineProps<Props>()
     >
       <slot />
     </span>
+    <span
+      class="nav-item__soon absolute top-60px xl:top-30px font-medium text-8px leading-14px text-white uppercase bg-white/60 rounded-4px px-12px"
+      v-if="soon"
+    >
+      Soon
+    </span>
   </div>
 </template>
 
 <style scoped>
-.nav-item .nav-item__line {
+.nav-item:not(.soon) .nav-item__line {
   transform-origin: left;
   transition: width 0.25s ease-in-out;
 }
-.nav-item:hover .nav-item__text {
+.nav-item:not(.soon):hover .nav-item__text {
   @apply font-semibold text-rust;
 }
-.router-link-active .nav-item .nav-item__text {
+.router-link-active .nav-item:not(.soon) .nav-item__text {
   @apply font-semibold text-white;
 }
-.router-link-active .nav-item:hover .nav-item__text {
+.router-link-active .nav-item:not(.soon):hover .nav-item__text {
   @apply font-semibold text-white;
 }
-.router-link-active .nav-item .nav-item__line
+.router-link-active .nav-item:not(.soon) .nav-item__line
 /* .nav-item:hover .nav-item__line */ {
   @apply w-full;
 }
