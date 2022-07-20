736bd796b77223575bfc8df5a9875216e1b41a38
Garfield550 <openpgp@550.moe>
2022-07-20 08:04:10 +0000

    feat: update contracts

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,2 +1,4 @@
 export type { AmbrusGenesisPeriod } from './AmbrusGenesisPeriod'
+export type { AmbrusStudioSaler } from './AmbrusStudioSaler'
 export { AmbrusGenesisPeriod__factory } from './factories/AmbrusGenesisPeriod__factory'
+export { AmbrusStudioSaler__factory } from './factories/AmbrusStudioSaler__factory'
