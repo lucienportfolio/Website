2bf4ccffe22ed89f8e14fe3b9f3d4f06f682f44e
xiaotian <istianlei@qq.com>
2022-07-20 20:43:04 +0800

    feat: html structure

---
+++ src/views/auction/AuctionView.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import PageMain from '@components/layout/PageMain.vue'
+</script>
+
+<template>
+  <PageMain>
+    <h2 class="text-white">
+      E4C Rangers NFT Series 1 <span class="text-rust">Ultimate Edition</span>
+    </h2>
+    <h1 class="text-white">auction event</h1>
+    <div class="bg-red w-[160px] h-[8px]"></div>
+
+    <div>swiper</div>
+
+    <div>
+      <div>
+        <div>
+          <h4>The Ultimate Utilities</h4>
+          <div>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+          </div>
+        </div>
+        <div>
+          <h4>The Ultimate Utilities</h4>
+          <div>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+            <section>
+              <p>10%</p>
+              <p>Revenue share of corresponding in-game hero/skin</p>
+            </section>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <span>Rin</span>
+            <span>Heir of Musashi</span>
+          </div>
+          <p>
+            Rin is the first ranger came to life in E4C Verse. We crafted 15 best-quality 3D arts as
+            our one-of-one NFTs, each having a meaningful background, the most special texture and
+            color combinations, and ultimately unique vibes. She’ll also make an appearance in our
+            upcoming games, E4C Final Salvation and E4C Fallen Arena.
+          </p>
+        </div>
+        <div>
+          <p>15 ULTIMATELY UNIQUE NFTs</p>
+          <button>RSVP for the auction</button>
+        </div>
+      </div>
+    </div>
+  </PageMain>
+</template>
