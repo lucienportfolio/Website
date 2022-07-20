45d2658ef0bf176adff27287476e5626d8648b1e
xiaotian <istianlei@qq.com>
2022-07-20 20:43:04 +0800

    feat: html structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'mint',
       component: () => import('@/views/mint/MintView.vue')
+    },
+    {
+      path: '/auction',
+      name: 'auction',
+      component: () => import('@/views/auction/AuctionView.vue')
     }
   ]
 })
