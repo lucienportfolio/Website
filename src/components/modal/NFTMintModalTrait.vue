9120f854f71d51b6feb02f3fe22abccded4db8db
Garfield550 <openpgp@550.moe>
2022-07-21 14:34:39 +0000

    fix: wrap hash string

--- src/components/modal/NFTMintModalTrait.vue
+++ src/components/modal/NFTMintModalTrait.vue
@@ -9,6 +9,6 @@ defineProps<Props>()
 <template>
   <div class="flex flex-col">
     <h5 class="font-normal text-12px leading-16px text-grey-dark uppercase mb-4">{{ title }}</h5>
-    <p class="font-bold text-16px leading-20px text-black-bg"><slot /></p>
+    <p class="font-bold text-16px leading-20px text-black-bg break-all"><slot /></p>
   </div>
 </template>
