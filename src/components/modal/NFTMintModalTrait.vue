62acf193d2324d7f73955721142f1d9475b6a8e9
Garfield550 <openpgp@550.moe>
2022-07-22 10:14:26 +0000

    feat: add explorer link on mint modal

--- src/components/modal/NFTMintModalTrait.vue
+++ src/components/modal/NFTMintModalTrait.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import ExternalLink from '../link/ExternalLink.vue'
 interface Props {
   title: string
+  link?: string
 }
 
 defineProps<Props>()
@@ -9,6 +11,14 @@ defineProps<Props>()
 <template>
   <div class="flex flex-col">
     <h5 class="font-normal text-12px leading-16px text-grey-dark uppercase mb-4">{{ title }}</h5>
-    <p class="font-bold text-16px leading-20px text-black-bg break-all"><slot /></p>
+    <ExternalLink
+      class="font-bold text-16px leading-20px text-black-bg break-all"
+      :to="link"
+      :blank="true"
+      v-if="link"
+    >
+      <slot />
+    </ExternalLink>
+    <p class="font-bold text-16px leading-20px text-black-bg break-all" v-else><slot /></p>
   </div>
 </template>
