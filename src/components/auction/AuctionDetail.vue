7ae1e4e403f0c51fd2bcba52ca4a65ad8558a1e5
xiaotian <istianlei@qq.com>
2022-07-25 16:38:16 +0800

    feat: style

--- src/components/auction/AuctionDetail.vue
+++ src/components/auction/AuctionDetail.vue
@@ -14,17 +14,19 @@ defineProps<Props>()
   >
     <div class="grid gap-y-9">
       <div v-for="(detail, index) in auction.detail" :key="index">
-        <h4 class="text-base font-semibold not-italic uppercase text-grey_1 mb-3">
+        <h4
+          class="text-xs md:text-base font-semibold not-italic uppercase text-grey_1 mb-3 leading-15px md:leading-20px"
+        >
           {{ detail.title }}
         </h4>
         <div class="xl:w-693px grid grid-cols-1 md:grid-cols-2 gap-3">
           <section
             v-for="(item, indexJ) in detail.introduction"
             :key="indexJ"
-            class="flex flex-col items-center px-6 py-3 bg-black/50 min-h-148px"
+            class="flex flex-col items-center px-6 py-3 bg-black/50 min-h-124px md:min-h-148px"
           >
             <p
-              class="text-64px font-bold text-center text-white leading-78px not-italic"
+              class="text-48px md:text-64px font-bold text-center text-white leading-59px md:leading-78px not-italic"
               :style="item.style"
             >
               {{ item.title }}
@@ -53,14 +55,14 @@ defineProps<Props>()
           v-html="auction.notes.content"
         ></p>
       </div>
-      <div class="bg-white p-9 text-center">
-        <p class="text-2xl not-italic font-bold text-rust leading-29px">
+      <div class="bg-white p-4 md:p-9 text-center fixed bottom-0 left-0 right-0 xl:static actions">
+        <p class="text-xl md:text-2xl not-italic font-bold text-rust leading-24px md:leading-29px">
           {{ auction.actions.text }}
         </p>
-        <a :href="auction.actions.link" target="_blank" rel="noopener">
+        <a :href="auction.actions.link" target="_blank" rel="noopener" class="inline-flex">
           <button
             :style="auction.actions.buttonStyle"
-            class="bg-rust text-2xl font-semibold uppercase text-white leading-29px p-y-5.5 p-x-19.25 mt-6"
+            class="bg-rust text-xl md:text-2xl font-semibold uppercase text-white leading-24px md:leading-29px p-y-4 md:p-y-5.5 p-x-9 md:p-x-19.25 mt-3 md:mt-6"
           >
             {{ auction.actions.button }}
           </button>
@@ -73,5 +75,12 @@ defineProps<Props>()
 <style scoped>
 .notes-content a {
   text-decoration: underline;
+}
+
+@media (max-width: 768px) {
+  .actions {
+    padding-bottom: calc(12px + constant(safe-area-inset-bottom));
+    padding-bottom: calc(12px + env(safe-area-inset-bottom));
+  }
 }
 </style>
--- src/components/auction/HeadTitle.vue
+++ src/components/auction/HeadTitle.vue
@@ -10,13 +10,15 @@ defineProps<Props>()
 
 <template>
   <div class="max-w-1332px p-x-9 mx-auto">
-    <h2 class="text-2xl uppercase text-white leading-29px mt-15 not-italic">
+    <h2
+      class="text-base md:text-2xl uppercase text-white leading-20px md:leading-29px mt-15 not-italic"
+    >
       {{ auction.subtitle }}
-      <span class="text-2xl font-bold uppercase text-rust leading-29px">{{
-        auction.subtitleFocus
-      }}</span>
+      <span class="font-bold uppercase text-rust block md:inline">{{ auction.subtitleFocus }}</span>
     </h2>
-    <h1 class="text-5xl font-bold uppercase text-white leading-59px mt-1 mb-3 not-italic">
+    <h1
+      class="text-4xl md:text-5xl font-bold uppercase text-white leading-44px md:leading-59px mt-1 mb-6 md:mb-3 not-italic"
+    >
       {{ auction.title }}
     </h1>
     <div class="w-40 h-2 bg-gradient-to-r from-red_1 to-rust"></div>
